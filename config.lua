a0daae32d947ae93518584c1aeda72ed07c0c40d
Dustin Schultz <dustindschultz@gmail.com>
2024-08-18 10:10:59 -0400

    added vim.g.dap_home variable

--- config.lua
+++ config.lua
@@ -2,4 +2,7 @@
 --  Uncomment this option if you want your OS clipboard to sync with Neovim.
 --  See `:help 'clipboard'`
 -- vim.opt.clipboard = 'unnamedplus'
+
+vim.g.dap_home = os.getenv 'HOME' .. '/src/'
+
 require 'user.font'
